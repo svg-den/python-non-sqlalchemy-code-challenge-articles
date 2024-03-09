a6bf33dd9ed165f7b071a93d0ed2859a88f9851b
svg-den <sundaywanjiru@gmail.com>
2024-03-09 18:33:58 +0300

    code challenge 2

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,90 @@
 class Article:
+    all = []
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
+        self._title = str(title)  
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, new_title):
+        return self._title
+        raise AttributeError("Title is immutable")
         
+
 class Author:
     def __init__(self, name):
-        self.name = name
-
+        self._name = name
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        self.new_name = new_name
+        return self._name
+    
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in self.articles()]))
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        return article
 
     def topic_areas(self):
-        pass
+        return list(set([article.magazine.category for article in self.articles()])) if self.articles() else None
+
+    def contributing_authors(self):
+        return [author for author in Author.all if len(author.articles()) > 0]
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
-
+        self._name = name
+        self._category = category
+    
+    @property
+    def name(self):
+        return self._name
+    @property
+    def category(self):
+        return self._category
+    @name.setter
+    def name(self,new_name):
+        if isinstance (new_name,str):
+            if len(new_name)>=2 and len(new_name) <=16:
+                self._name = new_name
+        return self._name
+    @category.setter
+    def category(self,new_category):
+        if isinstance (new_category,str):
+            if len(new_category) > 0:
+                self._category = new_category
+        return self._name
+    
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set([article.author for article in self.articles()]))
 
     def article_titles(self):
-        pass
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
 
     def contributing_authors(self):
-        pass
+        authors = {}
+        for article in self.articles():
+            if article.author in authors:
+                authors[article.author] += 1
+            else:
+                authors[article.author] = 1
+        contributing_authors = [author for author, count in authors.items() if count > 2]
+        return contributing_authors if contributing_authors else None
